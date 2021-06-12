b8e6bc9b41afdd70a618d763e848bb2e7b9c5bdf
Sam <sam.redmond@oceangoal.com>
2021-06-12 11:05:53 -0500

    Adding some basic button styling and forwarding event
    This adds some super basic styling and also forwards the on:click event so that it can be used by parent components. The slot also injects anything that the parent component puts in the Button html tag.

---
+++ src/components/button.svelte
@@ -0,0 +1,42 @@
+<script>
+</script>
+
+<!-- Blank method for on:click will pass that event to whatever component uses this one  -->
+<button on:click>
+  <!-- slot will cause button text to just be injected here -->
+  <slot />
+</button>
+
+<style>
+  button {
+    display: inline-block;
+    border: none;
+    padding: 1rem 2rem;
+    margin: 0;
+    text-decoration: none;
+    background: #ffffff;
+    color: #000000;
+    font-family: sans-serif;
+    font-size: 1rem;
+    cursor: pointer;
+    text-align: center;
+    transition: background 250ms ease-in-out, transform 150ms ease;
+    border: 2px solid black;
+    border-radius: 5px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
+
+  button:hover,
+  button:focus {
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  button:active {
+    transform: scale(0.99);
+  }
+</style>
