3edab625e97acfe80c4b1dddcc83ca33a9bd4626
Sam <sam.redmond@oceangoal.com>
2021-06-12 11:19:18 -0500

    Adding a checkbox with styling
    This checkbox just has styling but we will also need to manage state as far as checked and not checked because we will need that information in order to see if an item is done or not.

---
+++ src/components/checkbox.svelte
@@ -0,0 +1,73 @@
+<label class="container">
+  <slot />
+  <!-- If we want the checkbox to have a lavel -->
+  <input type="checkbox" />
+  <span class="checkmark" />
+</label>
+
+<style>
+  .container {
+    display: block;
+    position: relative;
+    padding-left: 35px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  .container input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+    border: 1px solid black;
+  }
+
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+  }
+
+  .container input ~ .checkmark {
+    border: 2px solid #000000;
+    background: #ffffff;
+  }
+
+  .container:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+
+  .container input:checked ~ .checkmark {
+    background-color: #000000;
+  }
+
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  .container input:checked ~ .checkmark:after {
+    display: block;
+  }
+
+  .container .checkmark:after {
+    left: 9px;
+    top: 5px;
+    width: 5px;
+    height: 10px;
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+</style>
