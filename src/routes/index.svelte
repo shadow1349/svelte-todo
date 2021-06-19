4e3193dd41a7d885783d9432bda241286a7c9945
Sam <sam.redmond@oceangoal.com>
2021-06-18 19:46:11 -0500

    Making the main page somewhat workable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Button from "../components/button.svelte";
+
+  import { items } from "../state";
+
+  let todo_items;
+
+  const unsubscribe = items.subscribe((values) => {
+    todo_items = values;
+  });
+</script>
+
+<h1>My Todo List</h1>
+
+<div class="button-container">
+  <Button>new todo item</Button>
+</div>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+  .button-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 0 50px 50px 50px;
+  }
+</style>
