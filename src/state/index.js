f50f84d75ee0589f37f9bce33b3992a17ceb08d7
Sam <sam.redmond@oceangoal.com>
2021-06-18 19:50:06 -0500

    adding a quick note to myself on how the items  should work

--- src/state/index.js
+++ src/state/index.js
@@ -1,3 +1,13 @@
 import { writable } from "svelte/store";
 
+/**
+ * An array of {content: string, complete: true, completedOn: number}
+ *
+ * content will be the string value of the todo item
+ * completed will be if the item is complete or not
+ * completedOn will be the exact time the item was completed on i.e. Date.now()
+ *
+ * We will move all completed items to the bottom of the array and order them by when
+ * they had been completed.
+ */
 export const items = writable([]);
