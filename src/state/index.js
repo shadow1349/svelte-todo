5abb3ef85a13842916cd3cfde05c7546c77f4d80
Sam <sredmond@techusolutions.com>
2021-06-30 18:36:24 -0500

    removing unused code

--- src/state/index.js
+++ src/state/index.js
@@ -4,5 +4,3 @@ import { writable } from "svelte/store";
  * An array of {id: number, content: string, complete: true, completedOn: number}
  */
 export const items = writable([]);
-
-export const completed = writable([]);
